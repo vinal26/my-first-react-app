a2dc1f85a6bec4f877289d667974518081f7ea2b
Vinal Patel <100340198+vinal26@users.noreply.github.com>
2023-07-12 10:18:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React For Free
         </a>
       </header>
     </div>
