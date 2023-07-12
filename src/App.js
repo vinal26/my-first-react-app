ccce4bbf406839ad4265fcc014b16c74d9cdfc45
vinal26 <100340198+vinal26@users.noreply.github.com>
2023-07-12 09:15:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React <h3> with </h3> vinal.
         </a>
       </header>
     </div>
